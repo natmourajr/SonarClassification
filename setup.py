12f44a6e73161ae4e45278a7e85e55c0e0fef133
Natanael Moura-Junior <natmourajr@gmail.com>
2022-12-21 12:30:41 -0300

    code updates

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Sonar Classification',
+    author='Natanael Nunes de Moura Junior',
+    license='MIT',
+)
